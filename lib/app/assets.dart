fe37d5f8f86dde95515d4820c9d5f905032eda32
Matthew Jones <mail@matthewrjones.me>
2024-01-09 20:44:54 -0500

    Created assets.dart

---
+++ lib/app/assets.dart
@@ -0,0 +1,15 @@
+class Assets {
+  static const applebees = 'restaurants/Applebees.png';
+  static const chipotle = 'restaurants/Chipotle.png';
+  static const crackerBarrel = 'restaurants/Cracker_Barrel.png';
+  static const dominos = 'restaurants/Dominos.png';
+  static const ihop = 'restaurants/iHop.png';
+  static const kfc = 'restaurants/KFC.png';
+  static const mcdonalds = 'restaurants/McDonalds.png';
+  static const oliveGarden = 'restaurants/Olive_Garden.png';
+  static const outback = 'restaurants/Outback.png';
+  static const paneraBread = 'restaurants/Panera_Bread.png';
+  static const subway = 'restaurants/Subway.png';
+  static const tacoBell = 'restaurants/Taco_Bell.png';
+  static const texasRoadhouse = 'restaurants/Texas_Roadhouse.png';
+}
