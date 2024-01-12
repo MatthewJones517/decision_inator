ff7cb858865a366175f30f9e1218de51b1814d69
Matthew Jones <mail@matthewrjones.me>
2024-01-11 19:51:18 -0500

    Added additional assets to assets.dart

--- lib/app/assets.dart
+++ lib/app/assets.dart
@@ -16,4 +16,40 @@ class Assets {
   static const subway = 'restaurants/Subway.png';
   static const tacoBell = 'restaurants/Taco_Bell.png';
   static const texasRoadhouse = 'restaurants/Texas_Roadhouse.png';
+
+  // Chores
+  static const bathroom = 'chores/bathroom.png';
+  static const cooking = 'chores/cooking.png';
+  static const dishes = 'chores/dishes.png';
+  static const dusting = 'chores/dusting.png';
+  static const groceries = 'chores/groceries.png';
+  static const laundry = 'chores/laundry.png';
+  static const trash = 'chores/trash.png';
+  static const vacuuming = 'chores/vacuuming.png';
+  static const windowCleaning = 'chores/window_cleaning.png';
+  static const yardWork = 'chores/yard_work.png';
+
+  // Dates
+  static const amusementPark = 'dates/amusement_park.png';
+  static const boardGames = 'dates/board_games.png';
+  static const comedyClub = 'dates/comedy_club.png';
+  static const karaoke = 'dates/karaoke.png';
+  static const movie = 'dates/movie.png';
+  static const museum = 'dates/museum.png';
+  static const painting = 'dates/painting.png';
+  static const picnic = 'dates/picnic.png';
+  static const triviaNight = 'dates/trivia_night.png';
+  static const walk = 'dates/walk.png';
+
+  // Streaming Services
+  static const appleTV = 'streaming_services/apple_tv.png';
+  static const crunchyroll = 'streaming_services/crunchyroll.png';
+  static const disneyPlus = 'streaming_services/disney_plus.png';
+  static const hboMax = 'streaming_services/hbo_max.png';
+  static const hulu = 'streaming_services/hulu.png';
+  static const netflix = 'streaming_services/netflix.png';
+  static const paramount = 'streaming_services/paramount.png';
+  static const peacock = 'streaming_services/peacock.png';
+  static const primeVideo = 'streaming_services/prime_video.png';
+  static const youTube = 'streaming_services/YouTube.png';
 }
